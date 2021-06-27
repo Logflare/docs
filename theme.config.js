ef5425c1f7771cba97fad8940a6eb78665970a3d
chasers <chase@logflare.app>
2021-06-27 09:16:40 -0700

    Change some config stuff to Logflare

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/logflare/docs',
+  titleSuffix: ' – Logflare Docs',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Logflare</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Logflare Docs
       </span>
     </>
   ),
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Logflare Docs" />
+      <meta name="og:description" content="Logflare Docs" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:site:domain" content="docs.logflare.app" />
+      <meta name="twitter:url" content="https://docs.logflare.app" />
+      <meta name="og:title" content="Logflare Docs" />
+      <meta name="og:image" content="" />
+      <meta name="apple-mobile-web-app-title" content="Logflare Docs" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -60,6 +60,6 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerEditOnGitHubLink: false,
+  footerText: <>MIT {new Date().getFullYear()} © Logflare.</>,
 }
